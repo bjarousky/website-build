c60ec0e14233ba6845be222bac169113ed74ca75
Brandon Jarousky <bjarousky@gmail.com>
2018-04-07 23:23:51 -0400

    js update

--- coded-files/main.js
+++ coded-files/main.js
@@ -1,1 +1,13 @@
 //JS will go here
+$(".nav-icon").on("click", function(){
+  $(".nav-items").toggleClass("nav-items-toggle");
+  $(".icon-bar").toggleClass("icon-change");
+});
+
+$(".nav-icon").on("mouseenter", function(){
+  $(".icon-bar").addClass("icon-bar-toggle");
+});
+
+$(".nav-icon").on("mouseleave", function(){
+  $(".icon-bar").removeClass("icon-bar-toggle");
+});
