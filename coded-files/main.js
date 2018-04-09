89797124b41e158984c1afdb343381aec2563e2d
Brandon Jarousky <bjarousky@gmail.com>
2018-04-08 23:08:08 -0400

    updated slide in and logo

--- coded-files/main.js
+++ coded-files/main.js
@@ -1,13 +1,23 @@
-//JS will go here
-$(".nav-icon").on("click", function(){
-  $(".nav-items").toggleClass("nav-items-toggle");
-  $(".icon-bar").toggleClass("icon-change");
-});
-
 $(".nav-icon").on("mouseenter", function(){
   $(".icon-bar").addClass("icon-bar-toggle");
 });
 
 $(".nav-icon").on("mouseleave", function(){
   $(".icon-bar").removeClass("icon-bar-toggle");
+});
+
+$(".nav-icon").on('click', function(){
+  $(".icon-bar").toggleClass("icon-change");
+  var $this = $(this);
+  if ($this.hasClass('open')) {
+    $(".nav-items").animate({
+      marginLeft : '100%'
+    }, 400);
+    $this.removeClass('open');
+  } else {
+    $(".nav-items").animate({
+      marginLeft : '0'
+    }, 400);
+    $this.addClass('open');
+  }
 });
